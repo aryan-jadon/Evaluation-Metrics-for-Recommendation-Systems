41b63536a1c818e58ec59d818844a71d5215b731
aryan-jadon <aryan.jadon@sjsu.edu>
2023-11-25 00:30:58 -0800

    Added Rating Metrics Files

---
+++ run_rating_metrics_experiments.sh
@@ -0,0 +1,4 @@
+# ==========================================================================================================================================
+### Workflow for movies 1m experiments
+
+python -m ranking_metrics.ranking_metrics_mse_experiment
