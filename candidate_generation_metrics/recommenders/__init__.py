64f47e22d86a04002023207e6ae28b5a26025294
aryan-jadon <aryan.jadon@sjsu.edu>
2023-11-27 11:53:02 -0800

    Added Files

---
+++ candidate_generation_metrics/recommenders/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Recommenders contributors.
+# Licensed under the MIT License.
+
+__title__ = "Recommenders"
+__version__ = "1.1.1"
+__author__ = "RecoDev Team at Microsoft"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018-present Recommenders contributors."
+
+# Synonyms
+TITLE = __title__
+VERSION = __version__
+AUTHOR = __author__
+LICENSE = __license__
+COPYRIGHT = __copyright__
