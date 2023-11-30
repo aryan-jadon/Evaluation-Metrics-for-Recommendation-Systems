2ea11bd9f82efb1776697c643b44021145168421
aryan-jadon <aryan.jadon@sjsu.edu>
2023-11-30 08:36:03 -0800

    Added Similarity Metrics Experiments Results

--- run_similarity_metrics_experiments.sh
+++ run_similarity_metrics_experiments.sh
@@ -2,21 +2,20 @@
 ### Workflow for amazon dataset electronics experiments
 
 ## Data Preparation for Benchmarking Experiment
-python -m similarity_metrics.parse.parse_json data/meta_Electronics.json.gz data/electronics.csv
-python -m similarity_metrics.prep.prep_node_relationship data/electronics.csv data/electronics_relationships.csv
-python -m similarity_metrics.prep.prep_edges data/electronics_relationships.csv data/electronics_edges.csv
-python -m similarity_metrics.prep.train_val_split data/electronics_edges.csv 0.33
-python -m similarity_metrics.prep.prep_graph_samples data/electronics_edges_train.edgelist data/electronics_sequences.npy electronics
+python -m similarity_metrics.parse.parse_json similarity_metrics/data/meta_Electronics.json.gz similarity_metrics/data/electronics.csv
+python -m similarity_metrics.prep.prep_node_relationship similarity_metrics/data/electronics.csv similarity_metrics/data/electronics_relationships.csv
+python -m similarity_metrics.prep.prep_edges similarity_metrics/data/electronics_relationships.csv similarity_metrics/data/electronics_edges.csv
+python -m similarity_metrics.prep.train_val_split similarity_metrics/data/electronics_edges.csv 0.33
+python -m similarity_metrics.prep.prep_graph_samples similarity_metrics/data/electronics_edges_train.edgelist similarity_metrics/data/electronics_sequences.npy electronics
 
 ## Benchmarking Similarity Metrics Experiments
-python -m similarity_metrics.torch_embedding_cosine_similarity data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_adjusted_cosine_similarity data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_chebyshev_distance data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_euclidean_distance data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_hamming_distance data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_jaccard_index data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_manhattan_distance data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_pearson_correlation_coefficient data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-python -m similarity_metrics.torch_embedding_spearman_rank_order_correlation_coefficient data/electronics_sequences.npy data/electronics_edges_val.csv  data/electronics_edges_val.csv 128 4
-
+python -m similarity_metrics.torch_embedding_cosine_similarity similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_adjusted_cosine_similarity similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_chebyshev_distance similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_euclidean_distance similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_hamming_distance similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_jaccard_index similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_manhattan_distance similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_pearson_correlation_coefficient similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
+python -m similarity_metrics.torch_embedding_spearman_rank_order_correlation_coefficient similarity_metrics/data/electronics_sequences.npy similarity_metrics/data/electronics_edges_val.csv  similarity_metrics/data/electronics_edges_val.csv 128 4
 # ==========================================================================================================================================
